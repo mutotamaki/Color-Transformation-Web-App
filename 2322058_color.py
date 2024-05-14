391d3ef7abbcd0ec5a8de538a3a630a5dfafb43d
Hondads <s2322058@stu.musashino-u.ac.jp>
2024-05-14 16:28:51 +0900

    imageをarrayに変更

--- 2322058_color.py
+++ 2322058_color.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 画像の正規化されたピクセル値が入った配列
-normalized_image = np.random.rand(100, 100)  # 仮の正規化された画像
+normalized_array = np.random.rand(100, 100)  # 仮の正規化された画像
 
 # 元のピクセル値の範囲を指定
 original_min = 0
 original_max = 255  # 例として、8ビットのグレースケール画像の場合
 
 # 正規化を元に戻す
-restored_image = (normalized_image * (original_max - original_min)) + original_min
+restored_image = (normalized_array * (original_max - original_min)) + original_min
 
 # 画像を表示
 plt.imshow(restored_image, cmap='gray')  # グレースケール画像の場合
